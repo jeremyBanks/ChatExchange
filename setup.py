901cc0c4393d420311ea32f6fef60f989c8c090e
Jeremy Banks <_@jeremy.ca>
2017-12-05 04:19:33 -0500

    m

--- setup.py
+++ setup.py
@@ -11,15 +11,21 @@ if __version__.endswith('-dev'):
     __version__ += str(int(datetime.datetime.utcnow().timestamp()))
 
 
-with open('Pipfile', 'rt') as f:
-    install_requires = list(toml.load(f)['packages'].keys())
-
-
 setup = lambda: setuptools.setup(
     name='stack.chat',
     version=__version__,
     python_requires='>=3',
-    install_requires=install_requires,
+    install_requires=[
+        'SQLAlchemy',
+        'lxml',
+        'html5lib',
+        'hashids',
+        'pprintpp',
+        'aiohttp',
+        'aiodns',
+        'cssselect',
+        'aitertools',
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
