7bbee5b697f4915ed1a4496e6fcd86cf1bc8f001
Jeremy Banks <_@jeremy.ca>
2017-12-05 04:13:13 -0500

    generate install_requires from Pipfile for now

--- setup.py
+++ setup.py
@@ -2,14 +2,24 @@ import datetime
 import setuptools
 
 
+import toml
+
+
+
 __version__ = '0.0.1-dev'
 if __version__.endswith('-dev'):
     __version__ += str(int(datetime.datetime.utcnow().timestamp()))
 
 
+with open('Pipfile', 'rt') as f:
+    install_requires = list(toml.load(f)['packages'].keys())
+
+
 setup = lambda: setuptools.setup(
     name='stack.chat',
     version=__version__,
+    python_requires='>=3',
+    install_requires=install_requires,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
