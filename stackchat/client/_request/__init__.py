defabaf60e5c6dc19b3b952833a83aafeaa8e5d5
ooronning <christopher.a.ronning@gmail.com>
2017-12-09 23:44:07 -0500

    updates

--- stackchat/client/_request/__init__.py
+++ stackchat/client/_request/__init__.py
@@ -188,15 +188,12 @@ class RoomMessages(_Request):
 class SendMessage(_Request):
     method = 'POST'
 
-    def _make_path(self, room_id):
+    def _make_path(self, room_id, text):
         self.room_id = int(room_id)
         return f'chats/{room_id}/messages/new'
 
-    def _make_data(self, text, fkey):
-        return {
-            'text': text,
-            'fkey': fkey
-        }
+    def _make_data(self, room_id, text):
+        return {'text': text}
 
 
 class TranscriptDay(_Request):
